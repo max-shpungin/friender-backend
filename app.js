e6683be8739b037e225d3649c5a01db24e3719d8
Max Shpungin <maxsdance@gmail.com>
2024-01-31 15:16:48 -0800

    added cors support

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 
 const { NotFoundError } = require("./expressError");
 
-const app = express();
 
 const userRoutes = require("./routes/userRoutes");
 
 
 
+const app = express();
+app.use(cors());
 // process JSON body => req.body
 app.use(express.json());
-
 
 app.use("/users", userRoutes);
 
